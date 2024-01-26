12efcfbc51c6c65435593b92b272022d0ed6ecac
HikiF2P <85029328+Hikikomori15@users.noreply.github.com>
2024-01-26 12:19:03 +0100

    Changed indentation size
    3rd Attempt at this

--- Blackjack.py
+++ Blackjack.py
@@ -7,28 +7,28 @@ Karten = [1, 2, 3 ,4 ,5, 6 ,7 ,8 ,9 ,10 ,11]
 Dealer, Dealer2, Dealer3, Spieler, Spieler2, Spieler3 = random.choice(Karten), random.choice(Karten), random.choice(Karten), random.choice(Karten), random.choice(Karten), random.choice(Karten)
 
 while True:
-   Alter = str(input("Bitte geben sie ihr Alter ein:  "))
-   try:
-      if Alter >= 18:
-         print ("Alt genug es geht weiter")
-         break
-      else:
-         print("Glücksspiel ist Ab 18, versuchen sie es nochmal")
-   except:
-      print("Bitte nächstes mal eine ganze zahl eingeben")
-      sys.exit(1)
+    Alter = str(input("Bitte geben sie ihr Alter ein:  "))
+    try:
+        if Alter >= 18:
+            print ("Alt genug es geht weiter")
+            break
+        else:
+            print("Glücksspiel ist Ab 18, versuchen sie es nochmal")
+    except:
+            print("Bitte nächstes mal eine ganze zahl eingeben")
+            sys.exit(1)
 
 while True:
-   Benutzername = str(input("Benutzer eingeben: "))
-   Passwort = str(input("Passwort eingeben:  "))
-   if Benutzername == "0":
-      if Passwort == "0":
-         print("Benutzername und Passwort Richtig")
-         break
-      else:
-         print("Benutzername oder Passwort Falsch")
-   else:
-      print("Benutzername oder Passwort Falsch")
+    Benutzername = str(input("Benutzer eingeben: "))
+    Passwort = str(input("Passwort eingeben:  "))
+    if Benutzername == "0":
+        if Passwort == "0":
+            print("Benutzername und Passwort Richtig")
+            break
+        else:
+            print("Benutzername oder Passwort Falsch")
+    else:
+        print("Benutzername oder Passwort Falsch")
 
 ##Wieso ein string mit einem input statt einen input selber?
 Spiel = str(input("Soll es Los gehen?: "))
@@ -41,23 +41,23 @@ Spieler4 = Spieler+ Spieler2
 Dealer4 = Dealer + Dealer2
 
 if Dealer4 <= 16:
-   print("Dealer muss eine Karte Ziehen!:" ) 
-   print(Dealer3 + Dealer2 + Dealer)
+    print("Dealer muss eine Karte Ziehen!:" ) 
+    print(Dealer3 + Dealer2 + Dealer)
 else:
-   print("Der Dealer muss nicht Ziehen!")
+    print("Der Dealer muss nicht Ziehen!")
 
 Dealer4 = Dealer + Dealer2 + Dealer
 Nachfrage = str(input("Willst du noch eine Karte Ziehen?: (Ja/Nein) "))
 while True:
-   if Nachfrage == "Ja":
-      print("\nDeine Hand")
-      print(Spieler + Spieler2 + Spieler3)
-      print("Dealers Hand")
-      print(Dealer + Dealer2 + Dealer3)
-      break
-   else:
-      print("\nDann Mal Sehen wer gewonnen Hat")
-   break
+    if Nachfrage == "Ja":
+        print("\nDeine Hand")
+        print(Spieler + Spieler2 + Spieler3)
+        print("Dealers Hand")
+        print(Dealer + Dealer2 + Dealer3)
+        break
+    else:
+        print("\nDann Mal Sehen wer gewonnen Hat")
+        break
 
 Spieler = Spieler + Spieler2 + Spieler3
 Dealer = Dealer + Dealer2 + Dealer3
@@ -67,16 +67,16 @@ time.sleep(3)
 
 print("\nSpieler:", Spieler, "\nDealer", Dealer)
 if Dealer >= 22:
-   print("Dealer hat überschritten, Du hast Gewonnen!")
+    print("Dealer hat überschritten, Du hast Gewonnen!")
 elif Spieler >= 22:
-   print("Du hast überschritten, Der  Dealer hat gewonnen!")
+    print("Du hast überschritten, Der  Dealer hat gewonnen!")
 elif Dealer < Spieler & Spieler <=21:
-   print("Du hast Gewonnen!")
+    print("Du hast Gewonnen!")
 elif Spieler < Dealer & Dealer <=21:
-   print("Dealer Gewinnt")
+    print("Dealer Gewinnt")
 elif Spieler == Dealer:
-   print("unentschieden")
+    print("unentschieden")
 else:
-   print("error")
+    print("error")
 
 sys.exit(1)
